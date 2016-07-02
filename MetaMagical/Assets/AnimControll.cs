53a9faf3b7e64de50164078b01bf211eb178e81f
seansaysmeh@gmail.com <seansaysmeh@gmail.com>
2016-07-02 14:22:40 +0100

    unifying two workspaces

---
+++ MetaMagical/Assets/AnimControll.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimControll : MonoBehaviour {
+
+    public Animator anim;
+
+    public bool idle;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        anim.SetBool("idle", true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
